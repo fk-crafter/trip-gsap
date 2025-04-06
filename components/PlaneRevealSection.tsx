2ed83f694ca4492cf96c9f5dff245af8da8b0bee
fk-crafter <fowad.kh@outlook.fr>
2025-04-06 15:03:16 +0200

    feat(PlaneReveal): added fades out effect

--- components/PlaneRevealSection.tsx
+++ components/PlaneRevealSection.tsx
@@ -14,49 +14,59 @@ export default function PlaneRevealSection() {
 
   useGSAP(() => {
     const ctx = gsap.context(() => {
-      gsap.fromTo(
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: sectionRef.current,
+          start: "top top",
+          end: "+=1500",
+          scrub: true,
+          pin: true,
+          anticipatePin: 1,
+        },
+      });
+
+      tl.fromTo(
         planeRef.current,
         {
           scale: 0.2,
-          y: 300,
           opacity: 0.5,
         },
         {
           scale: 1.5,
-          y: -880,
           opacity: 1,
           ease: "power2.out",
-          scrollTrigger: {
-            trigger: sectionRef.current,
-            start: "top top",
-            end: "+=1500",
-            scrub: true,
-            pin: true,
-            anticipatePin: 1,
-          },
+          duration: 0.5,
         }
-      );
+      ).to(planeRef.current, {
+        opacity: 0,
+        ease: "power1.out",
+        duration: 0.5,
+      });
     }, sectionRef);
 
     return () => ctx.revert();
   }, []);
 
   return (
-    <section
-      ref={sectionRef}
-      className="z-50 relative w-full h-screen flex items-center justify-center overflow-hidden"
-      style={{
-        background: "linear-gradient(to bottom, #dbeafe, #ffffff)", // sky blue to white
-      }}
-    >
-      <Image
-        ref={planeRef}
-        src="/plane.png"
-        alt="Flying Plane"
-        width={800}
-        height={800}
-        className="object-contain"
-      />
-    </section>
+    <>
+      <section
+        ref={sectionRef}
+        className="z-50 relative w-full h-screen flex items-center justify-center overflow-hidden"
+        style={{
+          background: "linear-gradient(to bottom, #dbeafe, #ffffff)",
+        }}
+      >
+        <Image
+          ref={planeRef}
+          src="/plane2.png"
+          alt="Flying Plane"
+          width={800}
+          height={800}
+          className="object-contain"
+        />
+      </section>
+
+      <section className="w-full h-[100vh] bg-white flex items-center justify-center"></section>
+    </>
   );
 }
